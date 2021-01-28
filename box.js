2928a77cc0d9a5ffa178f87bfef2a9f4998ce1b9
pradeepchandrasodadasu <74776284+pradeepchandrasodadasu@users.noreply.github.com>
2021-01-28 19:42:30 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic : false,
+            restitution : 0.8,
+            friction : 1.0,
+            density : 1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+    display(){
+        rectMode(CENTER);
+        fill("blue");
+        stroke("white");
+        strokeWeight(1.5);
+        rect(this.body.position.x, this.body.position.y, this.width, this.height);
+        
+    }
+}
